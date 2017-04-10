bb14aebae4f2f1a3d2c1347884e2dbdc78906b0a
EdricY <EdricY@users.noreply.github.com>
2017-04-10 03:05:38 -0400

    Final Draft (Turned in)

--- YuBellPlayer.h
+++ YuBellPlayer.h
@@ -24,6 +24,12 @@ class Ship {
 		bool operator<(const Ship &ship) const;
 };
 
+class Point {
+	public:
+		int row;
+		int col;
+};
+
 class YuBellPlayer: public PlayerV2 {
     public:
     	YuBellPlayer( int boardSize );
@@ -41,17 +47,33 @@ class YuBellPlayer: public PlayerV2 {
 
       int opponentsHits[MAX_BOARD_SIZE][MAX_BOARD_SIZE]; //where the opponent has shot
       int shipsPlaced[MAX_BOARD_SIZE][MAX_BOARD_SIZE]; //where we have placed ships this round
+			int shipPlacementScoring[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
       int currentRound; //how many rounds we have played up to this one
       void initializeProbMap(int probMap[MAX_BOARD_SIZE][MAX_BOARD_SIZE]); //populate a probability map with intial values
       void initializeShipsPlaced();
       Ship scoreShipPlacement(Ship ship);
       vector<Ship> getScoreAdjustedPositions(vector<Ship> positions);
       void updatePlacedShips(Ship ship);
+			vector<Point> hits;
+
+			Point findOpenSpaceRight(int row, int col);
+			Point findOpenSpaceLeft(int row, int col);
+			Point findOpenSpaceUp(int row, int col);
+			Point findOpenSpaceDown(int row, int col);
+			Point emptyPoint;
+			int killCount;
 
     	void initializeBoard();
       int lastRow;
       int lastCol;
 	    int numShipsPlaced;
+			int attackMap[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
+			int attackProbabilities[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
+			vector<int> shipLengths;
+			void missed(int row, int col);
+			bool onBoard(int x);
+			bool onBoard(int x, int y);
+			int getAttackMax();
       char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
 };
 
